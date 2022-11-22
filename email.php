3f04fe4a582a62d182213ade1ef4e7da710d6862
David Šajina <57251489+David-Sajina@users.noreply.github.com>
2022-11-22 18:33:48 +0100

    Fix

--- email.php
+++ email.php
@@ -10,7 +10,7 @@
  $naslov= $_POST['subject'];
  $message = $_POST['poruka'];
 
- $email_from = 'david.sajina@gmail.com';
+ $email_from = 'david.dapo.dd@gmail.com';
 
 	
 
@@ -21,7 +21,7 @@
      $headers = "From: $email_from \r\n";
 
      $headers .= "Reply-To: $visitor_email \r\n";
- mail("david.sajina@gmail.com",$naslov,$email_body,$headers)
+ mail("david.dapo.dd@gmail.com",$naslov,$email_body,$headers)
 ?>
 </body>
 </html>
